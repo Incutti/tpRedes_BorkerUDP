f516051bf54f12e86f5fd3724c57a449166834b9
Incutti <mateincu@gmail.com>
2023-09-01 09:00:46 -0300

    creamos la clase para hashear

---
+++ src/Seguridad/SHA.java
@@ -0,0 +1,30 @@
+package Seguridad;
+
+import com.sun.xml.internal.fastinfoset.algorithm.HexadecimalEncodingAlgorithm;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA{
+
+    public static String hashear(String mensajeOriginal){
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        // compute the hash of the input string
+        byte[] hash = md.digest(mensajeOriginal.getBytes());
+        // convert the hash to a hexadecimal string
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+
+    public static Boolean comprobacion(String hash1, String hash2){
+        return hash1.equals(hash2);
+    }
+}
